5083a2bce35dc07ca76251738290d143bfefd049
batman <IbNhAtTaB1986_>
2020-02-15 17:17:12 +0700

    HW3. Change project structure. Add Interface Storage

---
+++ src/com/urise/webapp/storage/Storage.java
@@ -0,0 +1,27 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public interface Storage {
+
+    void clear();
+
+    Resume get(String uuid);
+
+    void update(Resume resume);
+
+    void delete(String Uuid);
+
+    void save(Resume resume);
+
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll();
+
+    int size();
+}
