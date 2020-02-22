0ae3a3b073c5214ddaa89d5788c653118153eeb5
batman <IbNhAtTaB1986_>
2020-02-22 17:04:32 +0700

    HW4. Begin Testing. Call method toString with Reflection. Add Tests to ArrayStorage and SortedArrayStorage.

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -10,10 +10,14 @@ import static org.junit.Assert.*;
 
 public class AbstractArrayStorageTest {
 
-    private Storage storage = new ArrayStorage();
+    private final Storage storage;
     private final String UUID1 = "uuid1";
     private final String UUID2 = "uuid2";
     private final String UUID3 = "uuid3";
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @Before
     public void setUp() {
@@ -25,22 +29,39 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void clear() {
+        storage.clear();
+        assertEquals(storage.size(),0);
     }
 
     @Test
     public void update() {
+        Resume resume = new Resume(UUID1);
+        storage.update(resume);
+        Assert.assertEquals(resume, storage.get(resume.getUuid()));
     }
 
     @Test
     public void save() {
+        Resume resume = new Resume("uuid4");
+        try {
+            storage.get(resume.getUuid());
+        } catch (NotExistStorageException ex) {
+            System.out.println(ex.getMessage());
+        }
+        storage.save(resume);
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
+        Resume deletingResume = storage.get(UUID2);
+        storage.delete(deletingResume.getUuid());
+        storage.get(UUID2);
     }
 
     @Test
     public void get() {
+        Resume result = storage.get(UUID2);
+        Assert.assertEquals(result,new Resume(UUID2));
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -50,10 +71,13 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void getAll() {
+        Resume[] result = new Resume[]{new Resume(UUID1), new Resume(UUID2), new Resume(UUID3)};
+        Assert.assertEquals(result, storage.getAll());
     }
 
     @Test
     public void size() {
-        Assert.assertEquals(3,storage.size());
+        Assert.assertEquals(3, storage.size());
     }
+
 }
