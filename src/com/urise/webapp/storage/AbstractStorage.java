9143c36d92ed69e7e48e27cfebd62ce020a7d6de
batman <IbNhAtTaB1986_>
2020-03-02 21:33:36 +0700

    Add Generics and Logging support.

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,30 +6,36 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
     protected static final Comparator<Resume> COMPARATOR = Comparator.comparing(Resume::getFullName).
             thenComparing(Resume::getUuid);
 
     @Override
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         updateByPointer(getPointerIfExistElement(resume.getUuid()), resume);
     }
 
     @Override
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         insertIntoStorage(resume, getPointerIfNotExistElement(resume.getUuid()));
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         deleteElementByPointer(getPointerIfExistElement(uuid));
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return getResumeByPointer(getPointerIfExistElement(uuid));
     }
 
@@ -38,12 +44,14 @@ public abstract class AbstractStorage<SK> implements Storage {
         if (isExistPointer(pointer)) {
             return pointer;
         }
+        LOG.warning("Resume " + uuid + " not exist");
         throw new NotExistStorageException(uuid);
     }
 
     private SK getPointerIfNotExistElement(String uuid) {
         SK pointer = getPointerToResume(uuid);
         if (isExistPointer(pointer)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return pointer;
@@ -51,6 +59,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         return getList().stream().sorted(COMPARATOR).collect(Collectors.toList());
     }
 
