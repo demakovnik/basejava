2b6bf6c5ce6243eb831a3ea6e80cfe298f4b4a55
batman <IbNhAtTaB1986_>
2020-03-08 13:50:21 +0700

    HW9. Redid printListOfFiles() method with shifting. Applied the strategy pattern in classes that implement file storage.

--- src/com/urise/webapp/MainStorageTest.java
+++ src/com/urise/webapp/MainStorageTest.java
@@ -12,7 +12,7 @@ import java.io.InputStreamReader;
  * (just run, no need to understand)
  */
 public class MainStorageTest {
-    private final static Storage STORAGE = new PathStorage("storage",new ObjectToByteStreamComposerDecomposer());
+    private final static Storage STORAGE = new PathStorage("storage",new ObjectToByteStreamOperator());
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
