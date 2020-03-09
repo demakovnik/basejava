880a312555b231f2525ca358aaedf96ce9093cef
batman <IbNhAtTaB1986_>
2020-03-09 15:09:04 +0700

    HW9. Make fileoperator package and move FileStorageStrategy and ObjectToByteStreamOperator there. Refactor FileStorage and PathStorage classes.

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,7 +6,6 @@ import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Logger;
