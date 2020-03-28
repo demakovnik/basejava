2f69390b5c0ccdc9ea2613e317139a27427308f3
demakovnik <Nortumbria1986_>
2020-03-28 18:55:44 +0700

    HW12. Make int minValue(int[] values), List<Integer> oddOrEven(List<Integer> integers) methods.

--- src/com/urise/webapp/util/MainConcurrencyStreams.java
+++ src/com/urise/webapp/util/MainConcurrencyStreams.java
@@ -2,7 +2,6 @@ package com.urise.webapp.util;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.IntPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
