4a4c8f0bdcd77a71c09c40d4ec9289d9c6091146
demakovnik <Nortumbria1986_>
2020-03-29 09:42:30 +0700

    HW12. Make int minValue(int[] values), List<Integer> oddOrEven(List<Integer> integers) methods.

--- src/com/urise/webapp/util/MainConcurrencyStreams.java
+++ src/com/urise/webapp/util/MainConcurrencyStreams.java
@@ -2,43 +2,30 @@ package com.urise.webapp.util;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.IntPredicate;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static java.util.Comparator.*;
 
 public class MainConcurrencyStreams {
 
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 3, 2, 3};
+        int[] values = {1, 2, 3, 3, 2, 3, 4};
         List<Integer> sourceList = Arrays.stream(values).boxed().collect(Collectors.toList());
         System.out.println(minValue(values));
         System.out.println(oddOrEven(sourceList));
     }
 
     private static int minValue(int[] values) {
-        int stepen = 1;
-        int sum = 0;
-        Integer[] array = IntStream.of(values).distinct().boxed().sorted(reverseOrder()).toArray(Integer[]::new);
-        for (int i = 0; i < array.length; i++) {
-            sum += stepen * array[i];
-            stepen *= 10;
-        }
-        return sum;
+        AtomicInteger size = new AtomicInteger(values.length - 1);
+        return Arrays.stream(values).distinct().sorted().map(value -> (int) (value * Math.pow(10, size.decrementAndGet()))).map(v -> v / 100).sum();
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = integers.stream().flatMapToInt(IntStream::of).sum();
-        return sum % 2 == 0 ? integerList(integers, value -> value % 2 != 0) :
-                integerList(integers, value -> value % 2 == 0);
-    }
-
-    private static List<Integer> integerList(List<Integer> integers, IntPredicate predicate) {
         return integers
                 .stream()
                 .flatMapToInt(IntStream::of)
-                .filter(predicate)
+                .filter(num -> num % 2 != sum % 2)
                 .boxed()
                 .collect(Collectors.toList());
     }
