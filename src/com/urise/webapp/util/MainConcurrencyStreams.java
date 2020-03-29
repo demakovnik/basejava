1d76e19ed57f6e9c8c858e563a18a811a4a215d8
demakovnik <Nortumbria1986_>
2020-03-29 13:45:08 +0700

    HW12. Make int minValue(int[] values), List<Integer> oddOrEven(List<Integer> integers) methods.

--- src/com/urise/webapp/util/MainConcurrencyStreams.java
+++ src/com/urise/webapp/util/MainConcurrencyStreams.java
@@ -1,32 +1,35 @@
 package com.urise.webapp.util;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MainConcurrencyStreams {
 
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 3, 2, 3, 4};
+        int[] values = {4,1, 2, 3, 4, 3, 2, 3, 4};
         List<Integer> sourceList = Arrays.stream(values).boxed().collect(Collectors.toList());
         System.out.println(minValue(values));
         System.out.println(oddOrEven(sourceList));
     }
 
+
     private static int minValue(int[] values) {
-        AtomicInteger size = new AtomicInteger(values.length - 1);
-        return Arrays.stream(values).distinct().sorted().map(value -> (int) (value * Math.pow(10, size.decrementAndGet()))).map(v -> v / 100).sum();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (num1, num2)
+                -> num1*10 + num2);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = integers.stream().flatMapToInt(IntStream::of).sum();
         return integers
                 .stream()
-                .flatMapToInt(IntStream::of)
                 .filter(num -> num % 2 != sum % 2)
-                .boxed()
                 .collect(Collectors.toList());
     }
 }
